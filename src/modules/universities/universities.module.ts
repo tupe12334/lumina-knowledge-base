2d54eb7b138b66c21802425f080c733c7f18a7d6
tupe12334 <ofek.gabay.he@gmail.com>
2025-08-02 00:33:49 +0300

    refactor(universities): remove controller and related tests for universities module

--- src/modules/universities/universities.module.ts
+++ src/modules/universities/universities.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesService } from './universities.service';
-import { UniversitiesController } from './universities.controller';
 import { UniversitiesResolver } from './universities.resolver';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UniversitiesController],
+  controllers: [],
   providers: [UniversitiesService, UniversitiesResolver],
 })
 export class UniversitiesModule {}
