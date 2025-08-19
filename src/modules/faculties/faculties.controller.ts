058565c48ca7af04eebe9f5d2adc87402c2edb16
tupe12334 <ofek.gabay.he@gmail.com>
2025-08-20 00:54:22 +0300

    feat: Add Swagger API decorators to DTOs and Entities

--- src/modules/faculties/faculties.controller.ts
+++ src/modules/faculties/faculties.controller.ts
@@ -9,10 +9,16 @@ import {
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { FacultiesService } from './faculties.service';
 import { CreateFacultyInput } from './dto/create-faculty.input';
 import { UpdateFacultyInput } from './dto/update-faculty.input';
+import { Faculty } from './models/Faculty.entity';
 
 @ApiTags('faculties')
 @Controller('faculties')
@@ -20,26 +26,31 @@ export class FacultiesController {
   constructor(private readonly facultiesService: FacultiesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Faculty, description: 'Faculty created' })
   create(@Body() createFacultyDto: CreateFacultyInput) {
     return this.facultiesService.create(createFacultyDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: Faculty, isArray: true })
   findAll() {
     return this.facultiesService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Faculty })
   findOne(@Param('id') id: string) {
     return this.facultiesService.getFacultyById(id);
   }
 
   @Get('university/:universityId')
+  @ApiOkResponse({ type: Faculty, isArray: true })
   findByUniversity(@Param('universityId') universityId: string) {
     return this.facultiesService.getFacultiesByUniversity(universityId);
   }
 
   @Put(':id')
+  @ApiOkResponse({ type: Faculty })
   update(
     @Param('id') id: string,
     @Body() updateFacultyDto: Omit<UpdateFacultyInput, 'id'>,
@@ -49,6 +60,7 @@ export class FacultiesController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse({ description: 'Faculty deleted' })
   remove(@Param('id') id: string) {
     return this.facultiesService.delete(id);
   }
