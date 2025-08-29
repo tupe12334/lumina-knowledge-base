b4f4a362f697f57443b2454882ce47526950be46
tupe12334 <ofek.gabay.he@gmail.com>
2025-08-30 00:32:09 +0300

    feat: remove createBlockRelationship mutation and related input; simplify BlocksResolver

--- src/modules/blocks/blocks.resolver.ts
+++ src/modules/blocks/blocks.resolver.ts
@@ -1,7 +1,6 @@
 import { Resolver, Args, Mutation } from '@nestjs/graphql';
 import { BlocksService } from './blocks.service';
 import { Block } from './models/Block.entity';
-import { CreateBlockRelationshipInput } from './dto/create-block-relationship.input';
 import { DeleteBlockRelationshipInput } from './dto/delete-block-relationship.input';
 import { BlockRelationshipResult } from './dto/block-relationship-result.type';
 
@@ -12,22 +11,6 @@ import { BlockRelationshipResult } from './dto/block-relationship-result.type';
 @Resolver(() => Block)
 export class BlocksResolver {
   constructor(private readonly blocksService: BlocksService) {}
-
-  /**
-   * Creates a prerequisite/postrequisite relationship between two blocks.
-   * @param input - The relationship creation data
-   * @returns Promise<BlockRelationshipResult> The result of the operation
-   */
-  @Mutation(() => BlockRelationshipResult, {
-    name: 'createBlockRelationship',
-    description:
-      'Create a prerequisite/postrequisite relationship between blocks',
-  })
-  async createBlockRelationship(
-    @Args('input') input: CreateBlockRelationshipInput,
-  ): Promise<BlockRelationshipResult> {
-    return this.blocksService.createBlockRelationship(input);
-  }
 
   /**
    * Deletes a prerequisite/postrequisite relationship between two blocks.
