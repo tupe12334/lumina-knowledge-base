56663359bb5dbb81be08e6c1b4f8940f410e8470
tupe12334 <ofek.gabay.he@gmail.com>
2025-09-04 01:05:20 +0300

    refactor: remove InstitutionsResolver and related tests for universities

--- src/modules/institutions/institutions.module.ts
+++ src/modules/institutions/institutions.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InstitutionsService } from './institutions.service';
-import { InstitutionsResolver } from './institutions.resolver';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { InstitutionsController } from './institutions.controller';
 
 @Module({
   imports: [PrismaModule],
-  providers: [InstitutionsService, InstitutionsResolver],
+  providers: [InstitutionsService],
   controllers: [InstitutionsController],
 })
 export class InstitutionsModule {}
